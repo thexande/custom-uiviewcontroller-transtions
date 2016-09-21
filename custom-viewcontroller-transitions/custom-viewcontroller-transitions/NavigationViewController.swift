44e386d6ac72c3f0220311f11613dfee2df954b1
Joshua Alvarado <joshua@mondorobot.com>
2016-09-21 10:35:29 -0600

    added custom navigation controller

---
+++ custom-viewcontroller-transitions/custom-viewcontroller-transitions/NavigationViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NavigationViewController.swift
+//  custom-viewcontroller-transitions
+//
+//  Created by Joshua Alvarado on 9/21/16.
+//  Copyright © 2016 Joshua Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension NavigationViewController: UINavigationControllerDelegate {
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return CircleViewControllerAnimatedTransition(presenting: operation == .Pop)
+    }
+}
